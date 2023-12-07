97b71e16e0ef4a4b2eee949415d7775a03a19468
PacoCabralRV <151750456+PacoCabralRV@users.noreply.github.com>
2023-12-07 17:09:27 -0600

    Update main.cpp modificado

--- Animales/main.cpp
+++ Animales/main.cpp
@@ -1,64 +1,58 @@
 /*
-Erick Alan Garcia Muñoz y Francisco Cabral.
-Código elaborado en el examen 29-11-2023, hecho como examen.
+Erick Alan Garcia Muñoz and Francisco Cabral.
+Code developed on the exam date 29-11-2023, created as an exam.
 
-
-Universidad Panamericana
-clase de programación avanzada.
-
+Panamerican University
+Advanced programming class.
 */
 
-
-
-//Archivo main.cpp
+// File main.cpp
 #include <iostream>
 #include "Animal.h"
-#include "Perro.h"
-#include "Gato.h"
+#include "Dog.h"
+#include "Cat.h"
 using namespace std;
 
-//Definición de un macro
+// Definition of macros
 #define PI 3.1416
-#define Gravedad 9.81
+#define Gravity 9.81
 
 int main() {
-    //Creación de un objeto de la clase Animal
-    Animal a("Firulais", 5, 10.5, "Marrón");
-   
-    //Invocación de los métodos
-    a.comer();
-    a.dormir();
-    cout<< "El peso en Newtons de "<< a.getNombre() <<" es de : "<< a.getPeso()* Gravedad<<"N"<<endl;
+    // Creation of an object of the Animal class
+    Animal a("Firulais", 5, 10.5, "Brown");
 
-    cout<<"\n";
+    // Invocation of methods
+    a.eat();
+    a.sleep();
+    cout << "The weight in Newtons of " << a.getName() << " is: " << a.getWeight() * Gravity << "N" << endl;
 
-    //Creación de un objeto de la clase Perro
-    Perro p("Rocky", 3, 8.7, "Blanco", "Bulldog");
- 
-    //Invocación de los métodos
-    p.comer();
-    p.dormir();
-    p.ladrar();
-    p.proteger();
-    cout<<"El perro "<< p.getNombre() <<" es un "<< p.getRaza()<< " y su masa es de: "<<p.getPeso();
-    cout<<"\n";
+    cout << "\n";
 
-    //Creación de un objeto de la clase Gato
-    Gato g("Pirata", 2, 4.5, "Naranja", "Persa");
-    g.comer();
-    g.dormir();
-    g.maullar();
-    g.ronronear();
-    cout<<"La masa de "<< g.getNombre() <<" es de "<< g.getPeso()<< " y su peso en N es de: "<<p.getPeso()*Gravedad<<"N";
-    cout<<"\n";
+    // Creation of an object of the Dog class
+    Dog p("Rocky", 3, 8.7, "White", "Bulldog");
 
-    //Uso del macro para calcular el área de un círculo
-    cout<<"\n";
-    double radio = 2.5;
-    double area = PI * radio * radio;
-    cout << "El área de un círculo de radio donde se elaborara la casa de " <<g.getNombre() <<", tiene un radio de: "<< radio << "m, y su area es: " << area <<"m"<< endl;
+    // Invocation of methods
+    p.eat();
+    p.sleep();
+    p.bark();
+    p.protect();
+    cout << "The dog " << p.getName() << " is a " << p.getBreed() << " and its mass is: " << p.getWeight();
+    cout << "\n";
 
-  
+    // Creation of an object of the Cat class
+    Cat g("Pirata", 2, 4.5, "Orange", "Persian");
+    g.eat();
+    g.sleep();
+    g.meow();
+    g.purr();
+    cout << "The mass of " << g.getName() << " is " << g.getWeight() << " and its weight in N is: " << p.getWeight() * Gravity << "N";
+    cout << "\n";
 
-    return 0;
+    // Using the macro to calculate the area of a circle
+    cout << "\n";
+    double radius = 2.5;
+    double area = PI * radius * radius;
+    cout << "The area of a circle where the house of " << g.getName() << " will be built, has a radius of: " << radius << "m, and its area is: " << area << "m" << endl;
+
+    return 0;
 }
